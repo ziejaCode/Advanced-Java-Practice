0084399db822fbe3c49b6bc74cbd4fb56e5b6c9d
Marty <ziejacode@gmail.com>
2021-10-23 20:17:46 +0100

    commit 3

--- FirstProject/scr/app/app.java
+++ FirstProject/scr/app/app.java
@@ -3,7 +3,7 @@ package app;
 public class app {
     
     public static void main(String[] args) {
-        System.out.println("Hello  World Manny change for check");
+        System.out.println("Hello  World Manny change safasdfsdf for check");
     }
 }
 
