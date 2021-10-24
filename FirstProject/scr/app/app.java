f26593739bf856476eacc87d89e99e438106af89
Marty <ziejacode@gmail.com>
2021-10-24 10:40:30 +0100

    commit from eclipse made in vscode

--- FirstProject/scr/app/app.java
+++ FirstProject/scr/app/app.java
@@ -4,6 +4,11 @@ public class app {
     
     public static void main(String[] args) {
         System.out.println("Hello  World Manny change safasdfsdf for check");
+        System.out.println("This is another test");
+    }
+
+    public void anotherTest(){
+        System.out.println("This is another test");
     }
 }
 
